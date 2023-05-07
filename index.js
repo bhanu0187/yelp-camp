d7ada8ac5af2d1b7890035abd379c666ef9c1703
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-05-07 07:20:00 +0530

    Basic MongoDB schema setup

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.set("views", path.join(__dirname, "views"));
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/yelpCamp");
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error"));
+db.once("open", () => {
+	console.log("Databse connected");
+});
+
 app.get("/", (req, res) => {
 	res.render("home");
 });
