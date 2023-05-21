cc072dae1679a90ba5bfab2e6565306a05b7507f
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-05-21 06:17:44 +0530

    feat: Added navbar, footer and some styling

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const randomCities = Math.floor(Math.random() * cities.length);
+		const randomPrice = Math.floor(Math.random() * cities.length) + 10;
 		const camp = new Campground({
 			location: `${cities[randomCities].city}, ${cities[randomCities].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
+			image: "https://source.unsplash.com/collection/483251",
+			discription:
+				"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam porro magnam labore, molestiae sapiente similique voluptatem iusto. Quis nostrum inventore, eos obcaecati suscipit totam, expedita neque repellat corporis quidem incidunt odit itaque illum accusamus quos labore eveniet dolor eius atque?",
+			price: randomPrice,
 		});
 		await camp.save();
 	}
